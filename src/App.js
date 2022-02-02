75a769603bcd2e1405504f114f4bdfe8b332a09a
Alex <catalyst476@gmail.com>
2022-02-01 21:37:24 -0500

    Add interactions

--- src/App.js
+++ src/App.js
@@ -4,11 +4,51 @@ import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { API } from 'aws-amplify';
+import { listTodos } from './graphql/queries';
+import { createTodo as createTodoMutation, deleteTodo as deleteTodoMutation } from './graphql/mutations';
+
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
+const initialFormState = { name: '', description: '' }
+
 
 export default function App() {
+
+  const [formData, setFormData] = useState(initialFormState);
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listTodos });
+    setNotes(apiData.data.listNotes.items);
+  }
+
+  async function createNote() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createTodoMutation, variables: { input: formData } });
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteTodoMutation, variables: { input: { id } }});
+  }
+
+
+
+
   return (
+ 
+    <div className="App">
+    
     <Authenticator>
       {({ signOut, user }) => (
         <main>
@@ -16,6 +56,31 @@ export default function App() {
           <button onClick={signOut}>Sign out</button>
         </main>
       )}
+
+      <h1>My Notes App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+        {
+          notes.map(note => (
+            <div key={note.id || note.name}>
+              <h2>{note.name}</h2>
+              <p>{note.description}</p>
+              <button onClick={() => deleteNote(note)}>Delete note</button>
+            </div>
+          ))
+        }
+      </div>
     </Authenticator>
+  </div>
   );
 }
